a6995426cda34d68439204ac6135addd2094deaa
Simon DELAGE <sdelage@gmail.com>
2016-05-01 19:18:58 +0200

    Revert "Do nothing if both trace type are disabled"
    This reverts commit d79b276f5306029d05de46838b38b8b60027f1ae.

--- action.php
+++ action.php
@@ -19,10 +19,8 @@ require_once DOKU_PLUGIN.'action.php';
 class action_plugin_twistienav extends DokuWiki_Action_Plugin {
 
     public function register(Doku_Event_Handler $controller) {
-        if (($conf['youarehere']) or ($conf['breadcrumbs'])) {
-            $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'populate_jsinfo', array());
-            $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'handle_ajax_call', array());
-        }
+        $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'populate_jsinfo', array());
+        $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'handle_ajax_call', array());
     }
 
     function populate_jsinfo(&$event, $params) {
