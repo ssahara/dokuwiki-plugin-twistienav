014ffd6b9aeecce6149189954c048477d56743e3
Simon DELAGE <sdelage@gmail.com>
2016-05-16 02:02:53 +0200

    Fixed 'en' settings "lang" file

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,6 +5,6 @@
  * @author: Simon Delage <simon.geekitude@gmail.com>
  */
 
-$meta['twistieMap']         = 'Use map glyph for TwistieNavs showing wiki home index';
+$lang['twistieMap']         = 'Use map glyph for TwistieNavs showing wiki home index';
 $lang['pageIdTrace']        = 'Look for a [<code>pageId</code>] element and try to turn turn it into a short hierarchical trace with TwistieNavs.';
 $lang['pageIdExtraTwistie'] = 'Attempt to add an extra TwistieNav after [<code>pageId</code>] element that will show a popup with global index.';
