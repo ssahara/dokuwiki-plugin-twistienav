d1eca1909daadd0fca34bce70da12708f73b88b9
Simon DELAGE <sdelage@gmail.com>
2016-06-11 01:09:26 +0200

    Fix a settings typo

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -7,6 +7,6 @@
 
 $lang['startPagesOnly']     = 'Only add TwistieNavs to breadcrumbs leading to a namespace start page.';
 $lang['twistieMap']         = 'Use map glyph for TwistieNavs showing wiki home index.';
-$lang['pageIdTrace']        = 'Look for a [<code>pageId</code>] element and try to turn turn it into a short hierarchical trace with TwistieNavs.';
+$lang['pageIdTrace']        = 'Look for a [<code>pageId</code>] element and try to turn it into a short hierarchical trace with TwistieNavs.';
 $lang['pageIdExtraTwistie'] = 'Attempt to add an extra TwistieNav after [<code>pageId</code>] element that will show a popup with global index.';
 $lang['style']              = 'Use SVG images or Font Awesome glyphs. Font Awesome has to be externally installed (by the template or another plugin).';
