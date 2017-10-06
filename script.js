35fb436a8d75e36520362cc2788f759d3db9161d
Satoshi Sahara <sahara.satoshi@gmail.com>
2017-10-07 08:29:25 +0900

    replace a tab to four spaces

--- script.js
+++ script.js
@@ -10,23 +10,23 @@ var twistienav_plugin = {
     $callerObj: null,
 
     init: function () {
-		var $match = 0;
+        var $match = 0;
         if ((JSINFO['conf']['breadcrumbs'] > 0) && (jQuery('div.youarehere').length !== 0)) {
             twistienav_plugin.breadcrumbs('div.youarehere', 'yah_ns');
-			$match++;
+            $match++;
         }
         if ((JSINFO['conf']['youarehere'] == 1) && (jQuery('div.trace').length !== 0)) {
             twistienav_plugin.breadcrumbs('div.trace', 'bc_ns');
-			$match++;
+            $match++;
         }
         if ($match == 0) {
-			if ((JSINFO['conf']['breadcrumbs'] > 0) && (jQuery('div.breadcrumbs:has("span.bcsep")').length !== 0)) {
-				twistienav_plugin.breadcrumbs('div.breadcrumbs:has("span.bcsep")', 'bc_ns');
-			}
-			if ((JSINFO['conf']['youarehere'] == 1) && (jQuery('div.breadcrumbs:not(:has("span.bcsep"))').length !== 0)) {
-				twistienav_plugin.breadcrumbs('div.breadcrumbs:not(:has("span.bcsep"))', 'yah_ns');
-			}
-		}
+            if ((JSINFO['conf']['breadcrumbs'] > 0) && (jQuery('div.breadcrumbs:has("span.bcsep")').length !== 0)) {
+                twistienav_plugin.breadcrumbs('div.breadcrumbs:has("span.bcsep")', 'bc_ns');
+            }
+            if ((JSINFO['conf']['youarehere'] == 1) && (jQuery('div.breadcrumbs:not(:has("span.bcsep"))').length !== 0)) {
+                twistienav_plugin.breadcrumbs('div.breadcrumbs:not(:has("span.bcsep"))', 'yah_ns');
+            }
+        }
         if ((JSINFO['plugin_twistienav']['pit_skeleton'] != null) && (jQuery('div.pageId').length !== 0)) {
             twistienav_plugin.pageIdTrace('div.pageId', 'yah_ns');
         }
@@ -37,7 +37,7 @@ var twistienav_plugin = {
     /**
      * Add twisties and link events 
      */
-    breadcrumbs: function(div, ns_list){
+    breadcrumbs: function(div, ns_list) {
         var do_search;
         var $traceObj = jQuery(div);
         var $list = JSINFO['plugin_twistienav'][ns_list];
@@ -45,7 +45,7 @@ var twistienav_plugin = {
         jQuery(document).click(function(e) {
             twistienav_plugin.clear_results();
         });
-        
+
         do_search = function (caller, namespace) {
             twistienav_plugin.$callerObj = jQuery(caller);
             jQuery.post(
@@ -106,7 +106,7 @@ var twistienav_plugin = {
         jQuery(document).click(function(e) {
             twistienav_plugin.clear_results();
         });
-        
+
         do_search = function (caller, namespace) {
             twistienav_plugin.$callerObj = jQuery(caller);
             jQuery.post(
