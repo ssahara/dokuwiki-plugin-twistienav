b1a67ead4d17eafc3a18bbdf10e67a923e31a72d
Satoshi Sahara <sahara.satoshi@gmail.com>
2017-10-16 21:11:22 +0900

    description for exclusions config parameter

--- conf/default.php
+++ conf/default.php
@@ -9,7 +9,5 @@ $conf['enableTwistie'] = 'youarehere,breadcrumbs,pagebox';
 
 $conf['startPagesOnly']     = 1;
 $conf['exclusions']         = 'start,sidebar';
-//$conf['pageIdTrace']        = 1;
-//$conf['pageIdExtraTwistie'] = 1;
 $conf['style']              = 'svg';
 $conf['distinctHome']       = 1;
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -8,8 +8,6 @@
 $lang['enableTwistie'] = 'Choose where Twistie navigation to be enabled.';
 
 $lang['startPagesOnly']     = 'Only add TwistieNavs to breadcrumbs leading to a namespace start page.';
-$lang['exclusions']         = 'Only add TwistieNavs to breadcrumbs leading to namespaces containing more than any combination of these pages (you can add your own exclusions in the text field as a comma separated list like <code>topbar,sample</code>). These pages will also be excluded from popup indexes.';
-//$lang['pageIdTrace']        = 'Look for a [<code>pageId</code>] element and try to turn it into a short hierarchical trace with TwistieNavs.';
-//$lang['pageIdExtraTwistie'] = 'Attempt to add an extra TwistieNav after [<code>pageId</code>] element that will show a popup with global index.';
+$lang['exclusions']         = 'Pages to be excluded from popup index of relevant namespace. (you can add your own exclusions in the text field as a comma separated list like <code>topbar,sample</code>).';
 $lang['style']              = 'Use SVG images or Font Awesome glyphs. Font Awesome has to be externally installed (by the template or another plugin).';
 $lang['distinctHome']       = 'Use map glyph for TwistieNavs showing wiki home index.';
